820ec68e27626d4b4163e4f889b92d2f1e0af8be
ericareven <erica.reven@gmail.com>
2023-02-03 13:22:33 -0500

    visibility functions

--- js/app.js
+++ js/app.js
@@ -18,12 +18,13 @@ let answer = '';
 let maxWrong = 8;
 let guessed = [];
 let wordStatus = null;
-let mistakesLeft = 8
+let mistakesLeft = 8;
 
 // Play & Instruction Buttons
 function play() {
     document.getElementsByClassName("play").style.visibility = hidden
     document.getElementsByClassName("instructions").style.visibility = hidden
+    document.getElementById("letter").cursor = allowed
     // document.getElementsByClassName("letter-buttons").style.visibility = visible
 }
 
@@ -32,7 +33,7 @@ let playButton = document.querySelector(".play")
 playButton.addEventListener("click", play)
 
 function instruct() {
-    alert('Use the alphabet on screen to guess each letter in the hidden 5-letter word. But be careful, 8 wrong guesses and your cow will be abducted!')
+    alert('Use the alphabet below to guess each letter in the hidden 5-letter word. But be careful, 8 wrong guesses and your cow will be abducted!')
 }
 
 let insButton = document.querySelector(".instructions")
@@ -65,42 +66,60 @@ let letterButton
 const buttonElements = document.querySelectorAll('button');
 
 function checkLetters(){
-    mistakesLeft -= 1
-    document.getElementById("guesses").innerHTML = `Guesses: ${mistakesLeft}`
     if (wordsArr.includes(letterButton)) {
         guessed.push(letterButton)
         // console.log('y')
         updateGameboard()
         checkIfGameWon()
     } else {
+        mistakesLeft -= 1
+        document.getElementById("guesses").innerHTML = `Guesses: ${mistakesLeft}`
+        buildSpaceship()
             if(mistakesLeft === 0) {
-                gameOver()
+                setTimeout(() => {
+                    gameOver()
+            }, 2000)
             }
     }
+}
 
 function gameOver() {
     alert(`Game Over. The word was ${answer}`)
-}
+    reset()
 }
 
 function checkIfGameWon() {
     if (guessed.length === 5) {
         alert('You Won!')
-        setTimeout(() => {
-            reset()
-        }, 4000)
+        reset()
     }
 }
+
 function reset() {
     document.location.replace("/Users/erica.reven/Desktop/seir-eustoma/projects/project1/Alien-Abduction/index.html")
 }
 
-
+// Build space ship with each wrong guess
 function buildSpaceship(){
-    
+    if(mistakesLeft === 7) {
+        document.getElementById('pic-one').style.visibility = 'visible'
+    } else if(mistakesLeft === 6) {
+        document.getElementById('pic-two').style.visibility = 'visible'
+    } else if(mistakesLeft === 5) {
+        document.getElementById('pic-three').style.visibility = 'visible'
+    } else if(mistakesLeft === 4) {
+        document.getElementById('pic-four').style.visibility = 'visible'
+    } else if(mistakesLeft === 3) {
+        document.getElementById('pic-five').style.visibility = 'visible'
+    } else if(mistakesLeft === 2) {
+        document.getElementById('pic-six').style.visibility = 'visible'
+    } else if(mistakesLeft === 1) {
+        document.getElementById('pic-seven').style.visibility = 'visible'
+    } else if(mistakesLeft === 0) {
+        document.getElementById('pic-eight').style.visibility = 'visible'
+        document.getElementById('pic-nine').style.visibility = 'visible'
+    }
 }
-
-
 
 
 // Button event listener
@@ -108,8 +127,7 @@ buttonElements.forEach((element) => {
     element.addEventListener('click', function() {
         letterButton = element.attributes["data-key"].value
         console.log(letterButton)
-        element.style.backgroundColor = 'red';
-        // button.disabled = true;
+        // element.style.backgroundColor = 'red';
         checkLetters()
         element.disabled = true;
     })
